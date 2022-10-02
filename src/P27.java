d262c3d5580ffd5cb1362be14b6a622c2de1d94b
popstr-kun <hw38752920@gmail.com>
2022-10-02 16:59:41 +0800

    P27

---
+++ src/P27.java
@@ -0,0 +1,13 @@
+public class P27 {
+    public static void main(String[] args){
+        int num1,num2;
+
+        num1 = 3;
+
+        System.out.println("變數num1的值是:"+num1);
+
+        num2 = num1;
+
+        System.out.println("將 變數num1");
+    }
+}
